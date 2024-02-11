8d564391d1df1b543935c5f12f7d9c58c03056f4
cmfyoung <cmfyoung7@gmail.com>
2024-02-10 23:32:29 -0500

    Built readme outline, and wrote code for sign up not working yet

--- client/src/pages/Navbar/Navbar.jsx
+++ client/src/pages/Navbar/Navbar.jsx
@@ -1,31 +1,51 @@
+import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import logo from "../../assets/foodtruck.png";
 import { useState } from "react";
 
 const Navbar = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  console.log(isChecked);
 
-  const [isChecked, setIsChecked] = useState(false);  
-  console.log(isChecked)
+  const handleSignOut = () => {
+    console.log("User Signed out");
+    localStorage.removeItem("accessToken");
+    // Perform any other sign-out actions if needed
+  };
+
   return (
     <div className={styles.navbar}>
       <div className={styles.dropdown}>
         <div className={styles.hamburgerWrapper}>
-        <div className={styles.hamburgerPositioner}>
-          <input type="checkbox" id="hamburger" className={styles.hamburgerCheckbox} onChange={()=>{setIsChecked(!isChecked)}} />
-          <label htmlFor="hamburger" className={styles.hamburger}>
-            <span className={styles.line}></span>
-            <span className={styles.line}></span>
-            <span className={styles.line}></span>
-          </label>
+          <div className={styles.hamburgerPositioner}>
+            <input
+              type="checkbox"
+              id="hamburger"
+              className={styles.hamburgerCheckbox}
+              onChange={() => {
+                setIsChecked(!isChecked);
+              }}
+            />
+            <label htmlFor="hamburger" className={styles.hamburger}>
+              <span className={styles.line}></span>
+              <span className={styles.line}></span>
+              <span className={styles.line}></span>
+            </label>
+          </div>
         </div>
-      </div>
-        {isChecked && <div className={styles.dropdownContent}>
-          <a href="#">Home</a>
-          <a href="#">Favorites</a>
-          <a href="#">Login</a>
-          <a href="#">Sign Up</a>
-          <a href="#">Sign out</a>
-        </div>}
+        {isChecked && (
+          <div className={styles.dropdownContent}>
+            <Link to="/">Home</Link>
+            <Link to="/favorites">Favorites</Link>
+            <Link to="/reservations">Reservations</Link>
+            <Link to="/addtruck">Add Truck</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/sign-up">Sign Up</Link>
+            <a href="#" onClick={handleSignOut}>
+              Sign out
+            </a>
+          </div>
+        )}
       </div>
       <div className={styles.searchBar}>
         <input type="text" placeholder="Search..." />
